dd47886e8f4d8366f98a22d30f564a88a72b5b3b
valencaceres <vcaceres102@gmail.com>
2023-10-26 18:12:19 -0300

    Error en index arreglado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,12 @@
 import { type NextPage } from "next";
-<<<<<<< HEAD
-import Home from './home/index'
-=======
 import Home from "~/pages/home";
->>>>>>> 7e15e1968a6dfdeb2e38fb1545a04c331b8c3f34
 
 const Index: NextPage = () => {
   return (
-<<<<<<< HEAD
     <>
       <Home/>
     </>
     
-=======
-    <div>
-      <Home />
-    </div>
->>>>>>> 7e15e1968a6dfdeb2e38fb1545a04c331b8c3f34
   );
 };
 
